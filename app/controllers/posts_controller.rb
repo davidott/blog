b7050a516ee2efbec7ac5eaf3436c695b4bd7119
David Dotterweich <daviddotterweich@gmail.com>
2011-12-04 16:03:41 -0600

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,6 @@
+class PostsController < ApplicationController
+  def index
+    # pull things from the database
+    # render a view
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2e4fed8ed38497761be4a326794220a6ffaad1095babb700345612920daeaea0583aacb1b88f22f121264cd93e3f03babc18904ffbf4900e5ccb0adf2fca9216'
